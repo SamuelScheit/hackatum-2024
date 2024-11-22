fa6808d3feb9fc046d55558e96f99d382fe9d56f
Josef Georg Schmid <jgs@jws.de>
2024-11-23 00:55:16 +0100

    push and deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -J jumper@jumphost.hackatum.check24.de challenger@team13.hackatum.check24.de "cd ./hackatum-2024/server; git pull; chmod +x ./server/service.sh; sudo ./server/service.sh"
+ssh -J jumper@jumphost.hackatum.check24.de challenger@team13.hackatum.check24.de "cd ./hackatum-2024/server; git pull; chmod +x ./service.sh; sudo ./service.sh"
