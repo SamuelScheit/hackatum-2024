f45d0f0133fbcc5b10489712cbea484a01beeff1
Josef Georg Schmid <jgs@jws.de>
2024-11-23 00:55:01 +0100

    push and deploy

--- server/get_test.go
+++ server/get_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"math/rand"
 	"testing"
 )
@@ -14,21 +13,6 @@ func getRandomSortOrder() string {
 	}
 }
 
-func getRandomCarType() []byte {
-	switch rand.Intn(4) {
-	case 0:
-		return carTypeSmallBytes
-	case 1:
-		return carTypeSportsBytes
-	case 2:
-		return carTypeLuxuryBytes
-	case 3:
-		return carTypeFamilyBytes
-	}
-
-	return []byte{}
-}
-
 func BenchmarkSortOrder(b *testing.B) {
 	// run the Fib function b.N times
 	sortOrder := getRandomSortOrder()
@@ -37,26 +21,4 @@ func BenchmarkSortOrder(b *testing.B) {
 		b.Errorf("Invalid sort order")
 	}
 
-	getRandomSortOrder()
-}
-
-func BenchmarkCarType(b *testing.B) {
-	// run the Fib function b.N times
-	carTypeBytes := getRandomCarType()
-
-	var carType = -1
-
-	if bytes.Equal(carTypeBytes, carTypeSmallBytes) {
-		carType = carTypeSmall
-	} else if bytes.Equal(carTypeBytes, carTypeSportsBytes) {
-		carType = carTypeSports
-	} else if bytes.Equal(carTypeBytes, carTypeLuxuryBytes) {
-		carType = carTypeLuxury
-	} else if bytes.Equal(carTypeBytes, carTypeFamilyBytes) {
-		carType = carTypeFamily
-	} else if len(carTypeBytes) > 0 {
-		return
-	}
-
-	_ = carType
 }
