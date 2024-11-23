ff09c3ba00b9af39b69eafbbcf5a8cc73a31ee5a
Josef Georg Schmid <jgs@jws.de>
2024-11-23 19:32:36 +0100

    push and deploy

--- server/database/sql/create/insert-offer.sql
+++ server/database/sql/create/insert-offer.sql
@@ -1,4 +1,4 @@
 INSERT INTO Offers (
     ID, data, mostSpecificRegionID, startDate, endDate,
-    numberSeats, price, carType, hasVollkasko, freeKilometers, json
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    numberSeats, price, carType, hasVollkasko, freeKilometers, numberOfDays,  json
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
