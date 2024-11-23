3a42197e51be8b6bb14647d7eeaa994e03b1da41
Josef Georg Schmid <jgs@jws.de>
2024-11-23 18:24:00 +0100

    push and deploy

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -11,7 +11,6 @@ var API_OFFERS_PATH_BUFFER = []byte("/api/offers")
 
 // request handler in fasthttp style, i.e. just plain function.
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Println("Request URI:", string(ctx.URI().FullURI()))
 
 	if !bytes.Equal(ctx.Path(), API_OFFERS_PATH_BUFFER) {
 		ctx.Error("not found", fasthttp.StatusNotFound)
