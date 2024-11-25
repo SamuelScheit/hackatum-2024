432c261fb3352eb45ad06f0202ec25e913b60ece
samuelscheit <github@samuelscheit.com>
2024-11-25 23:31:03 +0100

    fix: concurrency

--- server/go.mod
+++ server/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/karask/go-avltree v0.0.0-20210208113816-739fb7fa1601 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.4.0 // indirect
 	github.com/tidwall/btree v1.7.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.57.0 // indirect
--- server/memory/linkedBtree.go
+++ server/memory/linkedBtree.go
@@ -1,25 +1,36 @@
 package memory
 
 import (
+	"sync"
+
+	"github.com/puzpuzpuz/xsync/v3"
 	"github.com/tidwall/btree"
 )
 
 type LinkedBtree struct {
 	root              btree.Map[int32, []int32]
-	cacheGreaterEqual map[int32]*BitArray
-	cacheLessThan     map[int32]*BitArray
-	cacheLessEqual    map[int32]*BitArray
+	cacheGreaterEqual *xsync.MapOf[int32, *BitArray]
+	cacheLessThan     *xsync.MapOf[int32, *BitArray]
+	cacheLessEqual    *xsync.MapOf[int32, *BitArray]
 	Size              int
+	mutex             sync.Mutex
 }
 
 func NewLinkedBtree() *LinkedBtree {
 
 	return &LinkedBtree{
-		root:              btree.Map[int32, []int32]{},
-		cacheGreaterEqual: map[int32]*BitArray{},
-		cacheLessThan:     map[int32]*BitArray{},
-		cacheLessEqual:    map[int32]*BitArray{},
-		Size:              0,
+		root: btree.Map[int32, []int32]{},
+		cacheGreaterEqual: xsync.NewMapOf[int32, *BitArray](
+			xsync.WithGrowOnly(),
+		),
+		cacheLessThan: xsync.NewMapOf[int32, *BitArray](
+			xsync.WithGrowOnly(),
+		),
+		cacheLessEqual: xsync.NewMapOf[int32, *BitArray](
+			xsync.WithGrowOnly(),
+		),
+		Size:  0,
+		mutex: sync.Mutex{},
 	}
 }
 
@@ -34,23 +45,27 @@ func (t *LinkedBtree) Add(key, value int32) {
 	t.root.Set(key, keys)
 	t.Size++
 
-	for k, v := range t.cacheGreaterEqual {
+	t.cacheGreaterEqual.Range(func(k int32, v *BitArray) bool {
 		if k >= key {
 			v.SetBit(int(value))
 		}
-	}
+		return true
+	})
 
-	for k, v := range t.cacheLessThan {
+	t.cacheLessThan.Range(func(k int32, v *BitArray) bool {
 		if k < key {
 			v.SetBit(int(value))
 		}
-	}
+		return true
+	})
 
-	for k, v := range t.cacheLessEqual {
+	t.cacheLessEqual.Range(func(k int32, v *BitArray) bool {
 		if k <= key {
 			v.SetBit(int(value))
 		}
-	}
+		return true
+	})
+
 }
 
 func (t *LinkedBtree) GreaterThan(key int32, fn func(int32, []int32)) {
@@ -91,13 +106,13 @@ func (t *LinkedBtree) LessThanEqual(key int32, fn func(int32, []int32)) {
 }
 
 func (t *LinkedBtree) BitArrayGreaterEqual(key int32) *BitArray {
-	if cache, exists := t.cacheGreaterEqual[key]; exists {
+
+	if cache, exists := t.cacheGreaterEqual.Load(key); exists {
 		return cache.Copy()
 	}
 
 	resBitArray := NewBitArray(t.Size)
-
-	t.cacheGreaterEqual[key] = resBitArray
+	t.cacheGreaterEqual.Store(key, resBitArray)
 
 	t.GreaterThanEqual(key, func(key int32, iids []int32) {
 		for _, iid := range iids {
@@ -109,13 +124,13 @@ func (t *LinkedBtree) BitArrayGreaterEqual(key int32) *BitArray {
 }
 
 func (t *LinkedBtree) BitArrayLessThan(key int32) *BitArray {
-	if cache, exists := t.cacheLessThan[key]; exists {
+	if cache, exists := t.cacheLessThan.Load(key); exists {
 		return cache.Copy()
 	}
 
 	resBitArray := NewBitArray(t.Size)
 
-	t.cacheLessThan[key] = resBitArray
+	t.cacheLessThan.Store(key, resBitArray)
 
 	t.LessThan(key, func(key2 int32, iids []int32) {
 		for _, iid := range iids {
@@ -126,13 +141,13 @@ func (t *LinkedBtree) BitArrayLessThan(key int32) *BitArray {
 }
 
 func (t *LinkedBtree) BitArrayLessEqual(key int32) *BitArray {
-	if cache, exists := t.cacheLessEqual[key]; exists {
+	if cache, exists := t.cacheLessEqual.Load(key); exists {
 		return cache.Copy()
 	}
 
 	resBitArray := NewBitArray(t.Size)
 
-	t.cacheLessEqual[key] = resBitArray
+	t.cacheLessEqual.Store(key, resBitArray)
 
 	t.LessThanEqual(key, func(key int32, iids []int32) {
 		for _, iid := range iids {
